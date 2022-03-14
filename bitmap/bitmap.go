7aaf6ef1042879967b0b9f2b602f5f80e65943b9
siliconhe <920504994@qq.com>
2022-03-14 14:33:55 +0800

    [feature]fix make slice overflow;add step alloc memory;add unit / bench test

--- bitmap/bitmap.go
+++ bitmap/bitmap.go
@@ -6,43 +6,67 @@
 
 package bitmap
 
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
 type Bitmap struct {
-	length int32
-	bitM []uint64
+	length int
+	bitM   []uint64
 }
 
 func NewBitmap() *Bitmap {
 	return &Bitmap{
 		length: 0,
-		bitM: []uint64{},
+		bitM:   make([]uint64, 128),
 	}
 }
 
-func (b *Bitmap) Set(num int) {
+func (b *Bitmap) Set(num int) error {
+	if num > math.MaxInt32 {
+		// https://github.com/golang/go/issues/38673
+		// easy get wrong for make a huge slice
+		return errors.New(fmt.Sprintf("Not support for length > %d(MaxInt32)", math.MaxInt32))
+	}
 	offset := num % 64
 	pos := num / 64
-	for pos >= len(b.bitM){
-		b.bitM = append(b.bitM,0)
+	diff := pos - len(b.bitM)
+
+	switch {
+	case diff > 0 && 2048 > diff:
+		b.bitM = append(b.bitM, make([]uint64, diff+64)...)
+
+	case diff >= 2048 && diff <= math.MaxInt32:
+		tempBitM := b.bitM
+		b.bitM = make([]uint64, pos+64)
+		copy(b.bitM, tempBitM)
 	}
-	if b.bitM[pos] & (1 << offset) == 0 {
+
+	if b.bitM[pos]&(1<<offset) == 0 {
 		b.bitM[pos] = b.bitM[pos] | (1 << offset)
-		b.length ++
-		return
+		b.length++
 	}
+	return nil
 }
 
 func (b *Bitmap) Get(num int) bool {
 	offset := num % 64
 	pos := num / 64
-	return len(b.bitM) > pos && b.bitM[pos] & (1 << offset) != 0
+	return len(b.bitM) > pos && b.bitM[pos]&(1<<offset) != 0
 }
 
-func (b *Bitmap)Clean(num int)  {
+func (b *Bitmap) Clean(num int) {
 	offset := num % 64
 	pos := num / 64
 	if pos > len(b.bitM) {
 		return
 	}
 	b.bitM[pos] = b.bitM[pos] & (^uint64(1 << offset))
-	return
+	b.length--
+}
+
+func (b *Bitmap) Length() int {
+	return b.length
 }
--- bitmap/bitmap_test.go
+++ bitmap/bitmap_test.go
@@ -7,19 +7,60 @@
 package bitmap
 
 import (
+	"math"
 	"testing"
 )
 
-func TestBitmap_Set(t *testing.T) {
+func TestBitmap_Function(t *testing.T) {
 	bitmap := NewBitmap()
-	bitmap.Set(123)
-	t.Log(bitmap.Get(123) == true)
-	t.Log(bitmap.Get(159) == false)
-	bitmap.Set(159)
-	t.Log(bitmap.Get(159) == true)
-	t.Log(bitmap.Get(1896557*15632) == false)
-	bitmap.Set(1896557*15632)
-	t.Log(bitmap.Get(1896557*15632) == true)
-	bitmap.Clean(1896557*15632)
-	t.Log(bitmap.Get(1896557*15632) == false)
+	bitmap.Set(1)
+	t.Log(bitmap.Get(1) == true)
+	t.Log(bitmap.Get(math.MaxInt8) == false)
+	bitmap.Set(math.MaxInt8)
+	bitmap.Set(math.MaxInt16)
+	t.Log(bitmap.Get(math.MaxInt16) == true)
+	bitmap.Set(math.MaxInt32)
+	t.Log(bitmap.Get(math.MaxInt32) == true)
+	bitmap.Clean(math.MaxInt32)
+	t.Log(bitmap.Get(math.MaxInt32) == false)
+	t.Logf(bitmap.Set(math.MaxInt64).Error())
+}
+
+func BenchmarkBitmap_Set(b *testing.B) {
+	bitmap := NewBitmap()
+	for i := 0; i < b.N; i++ {
+		bitmap.Set(1)
+		bitmap.Set(math.MaxInt8 / 2)
+		bitmap.Set(math.MaxInt8)
+
+		bitmap.Set(math.MaxInt16 / 2)
+		bitmap.Set(math.MaxInt16)
+
+		bitmap.Set(math.MaxInt32 / 2)
+		bitmap.Set(math.MaxInt32)
+	}
+}
+
+func BenchmarkBitmap_Get(b *testing.B) {
+	bitmap := NewBitmap()
+	bitmap.Set(1)
+	bitmap.Set(math.MaxInt8 / 2)
+	bitmap.Set(math.MaxInt8)
+
+	bitmap.Set(math.MaxInt16 / 2)
+	bitmap.Set(math.MaxInt16)
+
+	bitmap.Set(math.MaxInt32 / 2)
+	bitmap.Set(math.MaxInt32)
+	for i := 0; i < b.N; i++ {
+		bitmap.Get(1)
+		bitmap.Get(math.MaxInt8 / 2)
+		bitmap.Get(math.MaxInt8)
+
+		bitmap.Get(math.MaxInt16 / 2)
+		bitmap.Get(math.MaxInt16)
+
+		bitmap.Get(math.MaxInt32 / 2)
+		bitmap.Get(math.MaxInt32)
+	}
 }
